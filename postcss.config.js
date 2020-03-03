73694696c71386fc87616441e15ef22c174efe43
Sandro Daudishvili <sdaudishvili@gmail.com>
2020-03-03 21:20:05 +0400

    refactor postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,16 @@
-const getPlugins = () => {
-  const plugins = {
+const purgecss = {
+  '@fullhuman/postcss-purgecss': {
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+}
+
+module.exports = {
+  plugins: {
     'postcss-import': {},
     autoprefixer: {},
     tailwindcss: {},
-    'postcss-preset-env': { stage: 2 }
+    'postcss-preset-env': { stage: 2 },
+    ...(process.env.NODE_ENV === `production` ? purgecss : {})
   }
-  return process.env.NODE_ENV === 'production'
-    ? {
-        ...plugins,
-        '@fullhuman/postcss-purgecss': {
-          content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
-        }
-      }
-    : plugins
 }
-
-module.exports = { plugins: getPlugins() }
